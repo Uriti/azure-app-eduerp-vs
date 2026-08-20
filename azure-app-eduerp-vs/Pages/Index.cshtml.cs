3d13f4fab4dbcfefd67716a15aa739ad22f5f904
Uriti <sudheervuriti.net@outlook.com>
2026-08-20 11:55:30 +0530

    Added configurations greeting message

--- azure-app-eduerp-vs/Pages/Index.cshtml.cs
+++ azure-app-eduerp-vs/Pages/Index.cshtml.cs
@@ -5,9 +5,14 @@ namespace azure_app_eduerp_vs.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+        public IndexModel(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
         public void OnGet()
         {
-
+            ViewData["Greetings"] = _configuration["Greetings"];
         }
     }
 }
